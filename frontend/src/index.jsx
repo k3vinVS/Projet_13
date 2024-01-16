dfb8a0252a30180ad0fb858f37f20dccdf970eac
KEVIN VAN SCHELLEBECK <kevin.vanschellebeck@gmail.com>
2024-01-16 15:59:33 +0100

    refactoring

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(<React.StrictMode>hello world</React.StrictMode>);
