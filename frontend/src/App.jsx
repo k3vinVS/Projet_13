b50785080f81b2d398abb039db3bf39132503c3c
KEVIN VAN SCHELLEBECK <kevin.vanschellebeck@gmail.com>
2024-01-16 16:46:13 +0100

    setting up the react project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import User from "./pages/User";
+import Error from "./pages/Error";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<User />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
